29520cd6a894238b69bcfc11841fd723f72ee512
Anna <a.n.klochkova1@gmail.com>
2017-01-26 22:43:14 +0200

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,20 @@
+package task3;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i;
+		int j;
+		for (i = 2; i < 10; i++){		
+			for (j = 2; j < 10; j++){	
+		System.out.println(i + "x"+j+ "=" + i );
+	}
+
+}
+	}
+}
+
