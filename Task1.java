0ddbafbf59ec14f77a0b2352fc78162a6bf2351c
Anna <a.n.klochkova1@gmail.com>
2017-01-27 22:56:26 +0200

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,43 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		int n;
+		System.out.print("Input n");
+		n=sc.nextInt();
+		for(int j=0; j<n;j++){
+		for(int i=0; i<n;i++){
+			if(i%2==0){
+				System.out.print("+++");
+				}else{
+					System.out.print("***");
+				}
+		}
+		System.out.println();
+	}
+		}
+	}
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+
+
